090f8b161288ba9009389f3f5fef3114d3382539
José Coelho <jose.alberto.coelho@gmail.com>
2017-04-24 09:32:46 +0100

    Update skip-ci.groovy

--- skip-ci.groovy
+++ skip-ci.groovy
@@ -1,7 +1,7 @@
 #!groovy
 
-Boolean shouldSkip() {
-    sh(script: 'git log --pretty=oneline', returnStdout: true)
+Boolean shouldSkip(String commitId = "") {
+    sh(script: "git log --pretty=oneline ${commitId}", returnStdout: true)
         .split('\n')
         .any { it =~/(?i)\[skip\s+ci\]/ } 
 }
